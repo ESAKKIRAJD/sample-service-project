eb9a7348f1e797870c6cb6761bb929ed99df8fd8
ESAKKIRAJD <esakkiraj6394@gmail.com>
2018-01-28 15:37:04 +0530

    login

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    DashboardComponent,
+    ProfileComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'', pathMatch:'full', redirectTo:'login'},
+      { path:'login',component:LoginComponent },
+      { path:'forgotpassword',component:ForgotPasswordComponent},
+      { path:'dashboard',component:DashboardComponent },
+      { path:'profile',component:ProfileComponent },
+      { path:'register',component:RegisterComponent },
+
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
